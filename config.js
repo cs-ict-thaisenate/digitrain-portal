6649730a6b180a4ce4569bed2b6f295b2daa8c56
cs-ict-thaisenate <comp.science.senate@gmail.com>
2026-04-25 08:34:12 +0700

    Update GAS URL

--- config.js
+++ config.js
@@ -5,7 +5,7 @@
 
 const DigiTrain = {
   // ← วาง Google Apps Script Web App URL ที่นี่
-  GAS_URL: 'https://script.google.com/macros/s/YOUR_DEPLOYMENT_ID/exec',
+  GAS_URL: 'https://script.google.com/macros/s/AKfycbwvgiOKLxuHqTgnXh8-2s7gftgdrFvdEJ_S1ljmQ2D3BO9t6-0zDiMESJUtPJFqip4foQ/exec',
 
   // ตั้งค่า UI ที่โหลดจาก Config sheet ของ GAS
   config: {
